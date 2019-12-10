f55260bc12f764eaddf3e7a425cbd711258ccad7
Ángel Barrera Sánchez <angel@sighup.io>
2019-12-10 18:36:20 +0100

    Modularize minimal tests

---
+++ katalog/tests/istio/istio-minimal-install.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bats
+
+load ./../helper
+
+@test "testing init-istio package" {
+  info
+  test(){
+    apply katalog/istio/init
+    kubectl -n istio-system wait --for=condition=complete job --all
+  }
+  run test
+  [ "$status" -eq 0 ]
+}
+
+@test "testing istio package installation" {
+  info
+  test(){
+    apply katalog/istio
+  }
+  run test
+  [ "$status" -eq 0 ]
+}
+
+@test "wait for istio package to be running" {
+  info
+  test(){
+    retry_counter=0
+    max_retry=30
+    while kubectl get pods -n istio-system | grep -ie "\(Pending\|Error\|CrashLoop\|ContainerCreating\|PodInitializing\|Init:\)" >&2
+    do
+      [ $retry_counter -lt $max_retry ] || ( kubectl describe all -n istio-system >&2 && return 1 )
+      sleep 2 && echo "# waiting..." $retry_counter >&3
+      retry_counter=$[ $retry_counter + 1 ]
+    done
+  }
+  run test
+  [ "$status" -eq 0 ]
+}
--- katalog/tests/istio/istio-minimal.sh
+++ katalog/tests/istio/istio-minimal.sh
@@ -1,48 +1,6 @@
 #!/usr/bin/env bats
 
-apply (){
-  kustomize build $1 >&2
-  kustomize build $1 | kubectl apply -f - 2>&3
-}
-
-info(){
-  echo -e "${BATS_TEST_NUMBER}: ${BATS_TEST_DESCRIPTION}" >&3
-}
-
-@test "testing init-istio package" {
-  info
-  test(){
-    apply katalog/istio/init
-    kubectl -n istio-system wait --for=condition=complete job --all
-  }
-  run test
-  [ "$status" -eq 0 ]
-}
-
-@test "testing istio package installation" {
-  info
-  test(){
-    apply katalog/istio
-  }
-  run test
-  [ "$status" -eq 0 ]
-}
-
-@test "wait for istio package to be running" {
-  info
-  test(){
-    retry_counter=0
-    max_retry=30
-    while kubectl get pods -n istio-system | grep -ie "\(Pending\|Error\|CrashLoop\|ContainerCreating\|PodInitializing\|Init:\)" >&2
-    do
-      [ $retry_counter -lt $max_retry ] || ( kubectl describe all -n istio-system >&2 && return 1 )
-      sleep 2 && echo "# waiting..." $retry_counter >&3
-      retry_counter=$[ $retry_counter + 1 ]
-    done
-  }
-  run test
-  [ "$status" -eq 0 ]
-}
+load ./../helper
 
 @test "deploy bookinfo demo application" {
   info
